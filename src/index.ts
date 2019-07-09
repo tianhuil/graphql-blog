fee0fe4a356459187f6a5e3cea41d0fcbb345e0f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-07-08 20:32:47 -0700

    nexus server

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import * as path from 'path'
+import { GraphQLServer } from 'graphql-yoga'
+import { makePrismaSchema, prismaObjectType } from 'nexus-prisma'
+import { prisma } from './generated/prisma-client'
+import datamodelInfo from './generated/nexus-prisma'
+
+// @ts-ignore
+const Query = prismaObjectType({
+  name: 'Query',
+  definition: t => t.prismaFields(['*']),
+})
+const Mutation = prismaObjectType({
+  name: 'Mutation',
+  definition: t => t.prismaFields(['*']),
+})
+const User = prismaObjectType({
+  name: 'User',
+  definition: t => t.prismaFields(['id', 'createdAt', 'name'])
+})
+const Post = prismaObjectType({
+  name: 'Post',
+  definition(t) {
+    t.prismaFields([])
+  }
+})
+
+const schema = makePrismaSchema({
+  types: [Query, Mutation, User],
+  prisma: {
+    datamodelInfo,
+    client: prisma,
+  },
+  outputs: {
+    schema: path.join(__dirname, './generated/schema.graphql'),
+    typegen: path.join(__dirname, './generated/nexus.ts'),
+  },
+})
+
+const server = new GraphQLServer({
+  schema,
+  context: { prisma },
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
