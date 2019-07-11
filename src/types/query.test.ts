150722b7a9e730fc516d9210d1a7ce1220fd1242
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-07-10 22:32:54 -0700

    jest testing

---
+++ src/types/query.test.ts
@@ -0,0 +1,32 @@
+import { graphql } from 'graphql'
+
+import { schema } from '../schema'
+
+describe('Test Feed', () => {
+  const query = `
+  query {
+    feed {
+      title
+      text
+    }
+  }
+  `
+  const posts = [
+    { text: "a", title: "a" },
+    { text: "b", title: "b" }
+  ]
+
+  const mockPrisma = {
+    posts: () => posts
+  }
+
+  const context = { prisma: mockPrisma }
+
+  const variables = {}
+  test('a', async () => {
+    const { data } = await graphql(schema, query, null, context, variables)
+    expect( data ).toEqual({ feed: posts })
+  
+  }
+  )
+})
