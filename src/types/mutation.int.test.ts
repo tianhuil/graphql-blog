52142f5f0511e00e1ef159c6a5c071d7f09bebe2
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-07-11 21:55:24 -0700

    integration test

--- src/types/mutation.int.test.ts
+++ src/types/mutation.int.test.ts
@@ -6,35 +6,76 @@ import { prisma } from '../generated/prisma-client'
 describe('Test Mutations', () => {
   const context = { prisma }
 
-  test('Test signUp and creat', async () => {
-    const signUpResult = await graphql(schema, `
+  async function queryValidResults(source: string, variables: object): Promise<any> {
+    const result = await graphql(schema, source, null, context, variables)
+    expect(result.errors).toBeUndefined()
+    expect(result.data).toBeTruthy()
+    return result.data
+  }
+
+  beforeAll(async () => {
+    // Must be run in serial for relationship invariant to hold
+    prisma.deleteManyPosts()
+    prisma.deleteManyUsers()
+  })
+
+  test('Test signUp, create, and delete', async () => {    
+    const signUpData = await queryValidResults(`
     mutation {
       signUp(email: "example@example.com", password: "password", name: "Bob") {
         id
         createdAt
         name
       }
-    }`, null, context, {})
+    }`, {})
 
-    expect(signUpResult.errors).toBeUndefined()
-    expect(signUpResult.data).toBeTruthy()
-    expect(signUpResult.data!.signUp.name).toEqual("Bob")
-    const authorId = signUpResult.data!.signUp.id
+    expect(signUpData.signUp.name).toEqual("Bob")
+    const authorId = signUpData.signUp.id
     expect(authorId).toBeTruthy()
 
-    const createDraftResult = await graphql(schema, `
+    const createDraftData = await queryValidResults(`
     mutation {
       createDraft(title: "Title", authorId: "${authorId}") {
         id
         createdAt
         title
+        published
       }
-    }`, null, context, {})
+    }`, {})
 
-    expect(createDraftResult.errors).toBeUndefined()
-    expect(createDraftResult.data).toBeTruthy()
-    expect(createDraftResult.data!.createDraft.title).toEqual("Title")
-    const postId = createDraftResult.data!.createDraft.id
+    expect(createDraftData!.createDraft.title).toEqual("Title")
+    expect(createDraftData!.createDraft.published).toBe(false)
+    const postId = createDraftData!.createDraft.id
     expect( postId ).toBeTruthy()
+
+    const publishDraftData = await queryValidResults(`
+    mutation {
+      publishDraft(id: "${postId}") {
+        id
+        published
+      }
+    }`, {})
+
+    expect(publishDraftData.publishDraft.id).toEqual(postId)
+    expect(publishDraftData.publishDraft.published).toEqual(true)
+
+    const deletePostData = await queryValidResults(`
+    mutation {
+      deletePost(where: { id: "${postId}" }) {
+        id
+      }
+    }`, {})
+
+    expect(deletePostData.deletePost.id).toEqual(postId)
+
+    const postData = await queryValidResults(`
+    query {
+      post(id: "${postId}") {
+        id
+      }
+    }`, {})
+
+    expect(postData.post).toBeNull()
+
   })
 })
