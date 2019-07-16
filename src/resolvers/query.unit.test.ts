2f62392a39347bc9fcdfe49bf0b7c86b2aee72e5
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-07-15 22:31:24 -0700

    Fix query unit testing

--- src/resolvers/query.unit.test.ts
+++ src/resolvers/query.unit.test.ts
@@ -1,34 +1,33 @@
 import { graphql } from 'graphql'
 
 import { makeSchema } from '../schema'
-import { Prisma } from '../generated/prisma-client';
+import { Prisma, prisma } from '../generated/prisma-client';
 import { mockContext } from '../test-helpers';
 
-describe('Test Feed', () => {
+describe('Test Queries', () => {
   const schema = makeSchema()
 
-  const query = `
-  query {
-    feed {
-      title
-      text
-    }
-  }
-  `
-  const posts = [
-    { text: "a", title: "a" },
-    { text: "b", title: "b" }
-  ]
-
-  const mockPrisma = ({
-    posts: jest.fn().mockImplementation(() => posts)
-  } as unknown as Prisma)
-
-  const context = mockContext(mockPrisma)
-
-  const variables = {}
   test('Test feed', async () => {
-    const { data } = await graphql(schema, query, null, context, variables)
+    const query = `
+    query {
+      feed {
+        title
+        text
+      }
+    }
+    `
+    const posts = [
+      { text: "a", title: "a" },
+      { text: "b", title: "b" }
+    ]
+  
+    const mockPrisma = ({
+      posts: jest.fn().mockImplementation(() => posts)
+    } as unknown as Prisma)
+  
+    const context = mockContext({mockPrisma})
+
+    const { data } = await graphql(schema, query, null, context, {})
     expect( data ).toEqual({ feed: posts })
   })
 })
