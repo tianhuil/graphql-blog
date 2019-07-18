763a2ac065d2f1c05ad81650d29f799d1671de0d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-07-18 08:58:40 -0700

    Explicit typing of getUserId

--- src/server/context.ts
+++ src/server/context.ts
@@ -5,18 +5,20 @@ import { ContextParameters } from 'graphql-yoga/dist/types';
 export const makeContext = (params: ContextParameters) => {
   const auth = new Auth()
 
+  function getUserId(): string | null {
+    const authorization = params.request.get('Authorization')
+    if (authorization) {
+      const token = authorization.replace('Bearer ', '')
+      const verifiedToken = auth.verifyToken(token)
+      return verifiedToken && verifiedToken.userId
+    }
+    return null
+  }
+
   return {
     prisma,
     auth,
-    userId: (() => {
-      const authorization = params.request.get('Authorization')
-      if (authorization) {
-        const token = authorization.replace('Bearer ', '')
-        const verifiedToken = auth.verifyToken(token)
-        return verifiedToken && verifiedToken.userId
-      }
-      return null
-    })()
+    userId: getUserId()
   }
 }
 
