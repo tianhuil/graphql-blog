9790bf30aabab9c5d884bb52e1791b5ef8e6219c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-07-19 08:24:57 -0700

    refactor and test isPublished

--- src/middlewares/permissions.int.test.ts
+++ src/middlewares/permissions.int.test.ts
@@ -1,11 +1,8 @@
 import * as hashFunction from 'object-hash'
-import { prisma, User, Post } from '../generated/prisma-client'
-import { middleware } from 'graphql-middleware'
-import { isAuthenticated, isAuthor, isPublished, permissions } from './permissions'
-import { queryValidateResults, mockContext } from '../test-helpers';
-import { makeSchema } from '../server/schema';
-import { IShieldContext, IOptions, IRule } from 'graphql-shield/dist/types';
-import { isatty } from 'tty';
+import { prisma, Post } from '../generated/prisma-client'
+import { isAuthenticated, isAuthor, isPublished } from './permissions'
+import { mockContext } from '../test-helpers';
+import { IShieldContext, IOptions } from 'graphql-shield/dist/types';
 import { Rule } from 'graphql-shield/dist/rules';
 
 describe('Test Permissions', () => {
@@ -15,14 +12,18 @@ describe('Test Permissions', () => {
     name: "Permissions",
   }
 
-  const postData = {
-    title: "Permissions",
-    text: "Permissions",
+  const postData = [{
+    title: "Permissions 1",
+    text: "Permissions 1",
     published: false,
-  }
+  }, {
+    title: "Permissions 2",
+    text: "Permissions 2",
+    published: true,
+  }]
 
   let userId = ""
-  let postId = ""
+  let postIds: string[] = []
 
   beforeAll(async() => {
     try {
@@ -30,18 +31,22 @@ describe('Test Permissions', () => {
         ...userData
       })
       userId = user.id
-      const post = await prisma.createPost({
-        ...postData,
-        author: {
-          connect: { id: userId }
-        }
-      })
-      postId = post.id
     } catch(e) {
+      const user = await prisma.user({email: userData.email})
       // const errMessage = e.errors[0].message
       // "A unique constraint would be violated on User. Details: Field name = email"
       console.warn("User Already exists")
+      userId = user!.id
     }
+    const posts: Post[] = await Promise.all(
+      postData.map(postDatum => prisma.createPost({
+      ...postDatum,
+      author: {
+        connect: { id: userId }
+      }
+    }))
+  )
+  postIds = posts.map(post => post.id)
   })
 
   type ResolverArgs = {
@@ -79,18 +84,29 @@ describe('Test Permissions', () => {
   test('Test isAuthenticated', async() => {
     expect(await applyResolver(isAuthenticated, {})).toBeFalsy()
     expect(await applyResolver(isAuthenticated, { userId: "not the key"})).toBeFalsy()
+    console.log(userId)
     expect(await applyResolver(isAuthenticated, { userId })).toBeTruthy()
   })
 
   test('Test isAuthor', async() => {
     expect(await applyResolver(isAuthor, {})).toBeFalsy()
     expect(await applyResolver(isAuthor, { userId })).toBeFalsy()
-    expect(await applyResolver(isAuthor, { postId })).toBeFalsy()
-    expect(await applyResolver(isAuthor, { userId, postId, })).toBeTruthy()
+    postIds.forEach(async postId => {
+      expect(await applyResolver(isAuthor, { postId })).toBeFalsy()
+      expect(await applyResolver(isAuthor, { userId, postId })).toBeTruthy()
+    })
+  })
+
+  test('Test isPublished', async() => {
+    expect(await applyResolver(isPublished, {})).toBeFalsy()
+    expect(await applyResolver(isPublished, { postId: postIds[0] })).toBeFalsy()
+    expect(await applyResolver(isPublished, { postId: postIds[1] })).toBeTruthy()
   })
 
   afterAll(async() => {
-    await prisma.deletePost({id: postId})
+    await Promise.all(postIds.map(postId =>
+      prisma.deletePost({id: postId})
+    ))
     await prisma.deleteUser({id: userId})
   })
 })
